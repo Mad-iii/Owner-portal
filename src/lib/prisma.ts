83f2fefb15efae6b66961dea2db5a06aa0ef69e0
Mad-iii <mahdsadiq180@gmail.com>
2026-05-02 18:08:38 +0500

    fix prisma client import

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
@@ -9,7 +9,7 @@ function createPrismaClient() {
     const adapter = new PrismaPg({
         connectionString: process.env.DATABASE_URL!,
     });
-    return new PrismaClient({ adapter });
+    return new PrismaClient({ adapter } as any);
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
